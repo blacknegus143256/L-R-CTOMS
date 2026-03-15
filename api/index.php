996919186640bee9c37531c830da15303857d186
Patrick <patrick17cord@gmail.com>
2026-03-15 14:42:36 +0800

    Fixing Vercel API path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // This points from /api/index.php to /public/index.php
 require __DIR__ . '/../public/index.php';
